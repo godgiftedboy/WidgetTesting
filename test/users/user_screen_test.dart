05fb37ae31b4de08651373c5bba5c62da66c6c71
Raj Prajapati <waterflowtechnology@Waterflows-Mac-mini.local>
2025-02-03 14:03:38 +0545

    test: added simple widget test for loader and ListView

--- test/users/user_screen_test.dart
+++ test/users/user_screen_test.dart
@@ -1,5 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:widget_test_demo/users/user_model.dart';
+import 'package:widget_test_demo/users/user_screen.dart';
 
 void main() {
-  testWidgets('Displays list of users', (tester) async {});
+  testWidgets('Displays list of users', (tester) async {
+    final users = [
+      UserModel(id: 1, name: "raj", email: "raj@gmail.com"),
+      UserModel(id: 2, name: "suraj", email: "suraj@gmail.com"),
+    ];
+
+    Future<List<UserModel>> mockFetchUser() async {
+      await Future.delayed(const Duration(seconds: 1));
+      return users;
+    }
+
+    await tester.pumpWidget(MaterialApp(
+      home: HomeScreen(
+        futureUsers: mockFetchUser(),
+      ),
+    ));
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    //pump and settle for Futures and Animations
+    await tester.pumpAndSettle();
+    expect(find.byType(ListView), findsOneWidget);
+    expect(find.byType(ListTile), findsNWidgets(users.length));
+  });
 }
